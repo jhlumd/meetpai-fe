c9216b64345bd328b1a4fc472db15862296d600b
Jaehyuk Lee <jhlumd@gmail.com>
2020-10-16 11:29:42 -0700

    Remove unused files and add demo data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App(props) {
+  const { demoData } = props;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{demoData[0].name}</p>
+        <p>{demoData[1].name}</p>
       </header>
     </div>
   );
 }
-
-export default App;
---
+++ src/_demoData.js
@@ -0,0 +1,12 @@
+export const demoData = [
+  {
+    id: "r1",
+    type: "restaurant",
+    name: "Gaetano's Restaurant",
+  },
+  {
+    id: "m1",
+    type: "movie",
+    name: "The Good Doctor",
+  },
+];
