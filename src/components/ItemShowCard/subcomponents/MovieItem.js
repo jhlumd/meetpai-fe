00bf18ecb46208f946001cc8ae35e27e7138d8c4
Jaehyuk Lee <jhlumd@gmail.com>
2020-10-27 11:41:39 -0700

    Style movie item card

--- src/components/ItemShowCard/subcomponents/MovieItem.js
+++ src/components/ItemShowCard/subcomponents/MovieItem.js
@@ -1,13 +1,71 @@
 import React from "react";
+import { logoUrls } from "../../../demo/demoData";
 
 export default function MovieItem(props) {
   const { itemForShowCard } = props;
+  const {
+    name,
+    imgUrl,
+    genre,
+    releaseDate,
+    duration,
+    allRatings,
+    availableOn,
+    director,
+    cast,
+    synopsis,
+  } = itemForShowCard;
 
   return (
-    <div
-      className="item-show-card"
-    >
-      Movie: {itemForShowCard.name}
+    <div className="item-show-card">
+      <div className="poster-info">
+        <img src={imgUrl} className="movie-image" alt="movie poster" />
+
+        <div className="movie-info">
+          <h3>
+            {name}
+            <span className="release-date"> ({releaseDate})</span>
+          </h3>
+          <p className="genre-duration">
+            <span>{genre}</span>
+            <span>{duration}</span>
+          </p>
+          <p className="director">Directed by: {director}</p>
+        </div>
+      </div>
+
+      <div className="movie-ratings">
+        <div>
+          <p>{allRatings[0]}/10</p>
+          <p className="rating-site">IMDb</p>
+        </div>
+        <div className="rating-mid">
+          <p>{allRatings[1]}%</p>
+          <p className="rating-site">Rotten Tomatoes</p>
+        </div>
+        <div>
+          <p>{allRatings[2]}%</p>
+          <p className="rating-site">Metacritic</p>
+        </div>
+      </div>
+
+      <div className="synopsis">{synopsis}</div>
+
+      <div className="cast-list">
+        {cast.map((actor, i) => (
+          <div key={i} className="cast-card">
+            <img src="https://i.stack.imgur.com/YaL3s.jpg" alt="default cast profile" />
+            <p>{actor}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="movie-available-on">
+        <div className="movie-available-on-inner">
+          <p>Watch on:</p>
+          <img src={logoUrls[availableOn[0]]} alt={`${availableOn[0]} logo`} />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/ItemShowCard/subcomponents/RestaurantItem.js
+++ src/components/ItemShowCard/subcomponents/RestaurantItem.js
@@ -2,12 +2,40 @@ import React from "react";
 
 export default function RestaurantItem(props) {
   const { itemForShowCard } = props;
+  const {
+    name,
+    imgUrl,
+    cuisine,
+    location,
+    rating,
+    numRatings,
+    priceRange,
+    numBookedToday,
+    address,
+    phoneNumber,
+    availableOn,
+  } = itemForShowCard;
 
   return (
-    <div
-      className="item-show-card"
-    >
-      Restaurant: {itemForShowCard.name}
+    <div className="item-show-card">
+      <h3 className="restaurant-name">{name}</h3>
+      <img src={imgUrl} className="restaurant-image" alt="restaurant main" />
+
+      <div className="rating-price-range">
+        {rating}{numRatings}{priceRange}
+      </div>
+
+      <div className="cuisine-location">
+        {cuisine}{location}
+      </div>
+
+      <div className="booked-available-on">
+        {numBookedToday}{availableOn}
+      </div>
+
+      <div className="address-phone">
+        {address}{phoneNumber}
+      </div>
     </div>
   );
 }
